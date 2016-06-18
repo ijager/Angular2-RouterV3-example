8da662bca6fe1ba52301f7ff47bec0dda97dd441
Ingmar Jager <info@ingmarjager.nl>
2016-06-18 11:24:04 +0200

    Add simple components

---
+++ src/app/lista.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ROUTER_DIRECTIVES, Router } from '@angular/router';
+
+@Component({
+  moduleId: module.id,
+  selector: 'lista',
+  directives: [ROUTER_DIRECTIVES],
+  template:`
+  <h1>List A</h1>
+  <a class="link" *ngFor="let l of list" (click)="goToChild(l)"> Item {{l}} </a>
+  <br>
+  <h2> Child Routes: </h2>
+  <hr>
+  <router-outlet></router-outlet>
+  `,
+  styleUrls: ['lista.component.css']
+})
+
+export class ListAComponent implements OnInit {
+  list: any;
+
+  constructor(private router: Router) {
+    this.list = [1, 2, 3];
+  }
+
+  goToChild(l) {
+    console.log('clicked: ', l);
+    this.router.navigate(['/listA', l]);
+  }
+
+  ngOnInit() {}
+}
